6bfcd7b32d3a91a0b810c73de689ad88828fdcaf
PearlFoe <57188977+PearlFoe@users.noreply.github.com>
2020-09-29 18:49:09 +0300

    Add files via upload

--- actions.py
+++ actions.py
@@ -42,3 +42,8 @@ def login(driver, email=None, password=None):
 	actions.click(element).perform()
 
 	element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "profile-nav-item")))
+
+
+def logout(driver):
+	driver.get('https://www.linkedin.com/m/logout/')
+
--- main.py
+++ main.py
@@ -9,19 +9,9 @@ url = 'https://ru.linkedin.com/in/tayapopova/'
 def main():
 	driver = webdriver.Chrome()
 	actions.login(driver=driver, email=config.EMAIL, password=config.PASSWORD)
-	time.sleep(2)
-	person = Person(url=url, driver=driver)
-	actions.scroll_to_bottom(driver)
-	data = {
-		'name':person.get_name(),
-		'position':person.get_position(),
-		'company_name':person.get_company_name(),
-		'region':person.get_region(),
-		'photo_url':person.get_photo(),
-		'education':person.get_education()
-	}
-	print(data)
-	x=input()
+	time.sleep(15)
+
+	actions.logout(driver=driver)
 	driver.close()
 
 if __name__ == '__main__':
